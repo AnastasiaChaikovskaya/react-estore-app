75be584a958be8163692822ec60c889d528a2ae5
vladChaikovskyi <chaikovskyi@wesoftyou.com>
2024-04-04 21:22:12 +0300

    feat: add eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'jsx-a11y', 'react-hooks', 'import', '@typescript-eslint', 'markdown'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
