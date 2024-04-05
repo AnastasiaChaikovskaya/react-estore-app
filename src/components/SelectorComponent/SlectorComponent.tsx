8d6176534284e34f5b4198b0fb2157ef13a85421
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-04-05 03:32:54 +0300

    feat: finish styles for selector

--- src/components/SelectorComponent/SlectorComponent.tsx
+++ src/components/SelectorComponent/SlectorComponent.tsx
@@ -1,19 +1,30 @@
 import React, { FC } from 'react';
+import '@/components/SelectorComponent/SelectorComponent.scss';
+import IconComponent from '../IconComponent';
 
 type Props = {
-  plaseholder: string;
+  placeholder: string;
   items: string[];
+  width: number;
 };
 
-const SelectorComponent: FC<Props> = ({ plaseholder, items }) => {
+const SelectorComponent: FC<Props> = ({ placeholder, items, width }) => {
   return (
-    <div className="selector">
-      <button className="selector__button">{plaseholder}</button>
-      {items.map((item, index) => (
-        <div className="selector__item" key={index}>
-          {item}
-        </div>
-      ))}
+    <div className="selector" style={{ width: width }}>
+      <label className="selector__name">{placeholder}</label>
+
+      <button className="selector__button">
+        <span className="selector__placeholder">{placeholder}</span>
+        <IconComponent name="arrow-down" width={16} height={16} color="#B4BDC3" />
+      </button>
+
+      <div className="selector__menu">
+        {items.map((item, index) => (
+          <div className="selector__item" key={index}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
