3e054bef4de283b7d73e823fa37fc63f87a8981c
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-04-07 22:13:13 +0300

    feat: fix button types

--- src/components/ButtonComponent/Button.tsx
+++ src/components/ButtonComponent/Button.tsx
@@ -1,23 +1,15 @@
 import React, { FC } from 'react';
-import { BUTTON_SIZE, BUTTON_VARIANTS } from '@/components/ButtonComponent/types';
 import './Button.scss';
 import { clsx } from 'clsx';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: BUTTON_VARIANTS;
-  size?: BUTTON_SIZE;
+  variant?: 'default' | 'circle' | 'primary' | 'link';
+  size?: 'sm' | 'md' | 'lg';
   iconLeft?: React.ReactElement;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const {
-    variant = BUTTON_VARIANTS.DefaultButton,
-    size = BUTTON_SIZE.Medium,
-    iconLeft,
-    className,
-    children,
-    ...restProps
-  } = props;
+  const { variant = 'default', size = 'md', iconLeft, className, children, ...restProps } = props;
 
   return (
     <button className={clsx('btn', `btn--${variant}`, `btn--${size}`, className)} {...restProps}>
--- src/pages/main/PhonesPage.tsx
+++ src/pages/main/PhonesPage.tsx
@@ -1,24 +1,17 @@
 import Button from '@/components/ButtonComponent/Button';
-import { BUTTON_SIZE, BUTTON_VARIANTS } from '@/components/ButtonComponent/types';
 import IconComponent from '@/components/IconComponent';
 import React from 'react';
 
 function PhonesPage() {
   return (
     <>
-      <Button
-        variant={BUTTON_VARIANTS.BackButton}
-        size={BUTTON_SIZE.Large}
-        iconLeft={<IconComponent name="arrow-left" />}
-      >
-        Primary
+      <Button variant="link" size="sm" iconLeft={<IconComponent name="arrow-left" />}>
+        sdfsdfsd
       </Button>
 
-      <Button
-        variant={BUTTON_VARIANTS.DefaultButton}
-        size={BUTTON_SIZE.Small}
-        iconLeft={<IconComponent name="heart" />}
-      ></Button>
+      <Button variant="default" size="sm">
+        <IconComponent name="heart" />
+      </Button>
     </>
   );
 }
