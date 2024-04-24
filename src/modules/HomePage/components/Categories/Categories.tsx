fde83e092f5efb88465bc6db2c85a5aacedf79b9
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-04-24 16:09:52 +0300

    feat: start categories component

---
+++ src/modules/HomePage/components/Categories/Categories.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import Heading from '@/components/Heading/Heading';
+import Text from '@/components/Text/Text';
+import { TProduct } from '@/modules/shared/ProductItem/products';
+import Accessories from '@/assets/categories/phones.png';
+import Tablets from '@/assets/categories/tablets.png';
+import Phones from '@/assets/categories/accesories.png';
+import '@/modules/HomePage/components/Categories/Categories.scss';
+
+const Categories = () => {
+  const [quantityOfPhones, setQuantityOfPhones] = useState(0);
+  const [quantityOfTablets, setQuantityOfTablets] = useState(0);
+  const [quantityOfAccessories, setQuantityOfAccessories] = useState(0);
+  const getProducts = async () => {
+    try {
+      const response = await fetch('/api/products.json');
+      const productsFromServer: TProduct[] = await response.json();
+
+      const quantityOfPhonesServer = productsFromServer.filter((item) => item.category === 'phones').length;
+      setQuantityOfPhones(quantityOfPhonesServer);
+
+      const quantityOfTabletsFromServer = productsFromServer.filter((item) => item.category === 'tablets').length;
+      setQuantityOfTablets(quantityOfTabletsFromServer);
+
+      const quantityOfAccessoriesFromServer = productsFromServer.filter(
+        (item) => item.category === 'accessories',
+      ).length;
+      setQuantityOfAccessories(quantityOfAccessoriesFromServer);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div className="container">
+      <div className="categories">
+        <Heading tag="h2" size="2" align="left" className="categories__title" weight="bold">
+          Shop by category
+        </Heading>
+        <div className="categories__wrapper">
+          <div className="category">
+            <div className="category__bg-phones">
+              <img src={Phones} alt="/" className="category__img" />
+            </div>
+            <div className="category__info">
+              <Heading tag="h4" size="4" align="left" weight="semi-bold" className="category__title">
+                Mobile phones
+              </Heading>
+              <Text tag="p" size="base" align="left">
+                {`${quantityOfPhones} models`}
+              </Text>
+            </div>
+          </div>
+
+          <div className="category">
+            <div className="category__bg-tablets">
+              <img src={Tablets} alt="/" className="category__img" />
+            </div>
+            <div className="category__info">
+              <Heading tag="h4" size="4" align="left" weight="semi-bold" className="category__title">
+                Tablets
+              </Heading>
+              <Text tag="p" size="base" align="left">
+                {`${quantityOfTablets} models`}
+              </Text>
+            </div>
+          </div>
+
+          <div className="category">
+            <div className="category__bg-accessories">
+              <img src={Accessories} alt="/" className="category__img" />
+            </div>
+            <div className="category__info">
+              <Heading tag="h4" size="4" align="left" weight="semi-bold" className="category__title">
+                Accessories
+              </Heading>
+              <Text tag="p" size="base" align="left">
+                {`${quantityOfAccessories} models`}
+              </Text>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/pages/main/HomePage.tsx
+++ src/pages/main/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from '@/modules/HomePage/components/HeroSection/HeroSection';
 import NewModelSection from '@/modules/HomePage/components/NewModelSection/NewModelSection';
+import Categories from '@/modules/HomePage/components/Categories/Categories';
 
 const items = ['1', '2'];
 
@@ -9,6 +10,7 @@ function HomePage() {
     <>
       <HeroSection />
       <NewModelSection />
+      <Categories />
     </>
   );
 }
